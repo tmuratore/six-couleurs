b3af16b35f152978bc1b90a89a2d85c1da7a3a5f
aurelienshz <aurelien.schiltz@free.fr>
2016-04-02 14:46:24 +0200

    Prevent the first tiles controlled from being the same color

--- 6Colors/src/edu/isep/sixcolors/SixColors.java
+++ 6Colors/src/edu/isep/sixcolors/SixColors.java
@@ -3,6 +3,10 @@ import edu.isep.sixcolors.controller.*;
 import edu.isep.sixcolors.model.*;
 import edu.isep.sixcolors.view.*;
 
+import java.util.Random;
+
+// TODO factorize random color generation (duplicated in this and model.Board)
+
 /**
  * TODO annotate
  */
@@ -13,6 +17,7 @@ public class SixColors {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Random random = new Random();
 		
 		Game game = new Game();
 		
@@ -22,14 +27,19 @@ public class SixColors {
 			game.getPlayer(i).setName(name);
 		}
 		
+		// TODO store board width as an independant parameter
+		
 		Board board = new Board(4);
 		
 		board.getTile(0, 0).setOwner(game.getPlayer(0));
+		while(board.getTile(3, 3).getColor() == board.getTile(0, 0).getColor()) {
+			board.getTile(3, 3).setColor(Colors.values()[random.nextInt(Colors.values().length)]);
+		}
+		
 		board.getTile(3, 3).setOwner(game.getPlayer(1));
 		
 		// TODO let players choose their colors
 		// TODO allow for more than 2 players
-		// TODO enclose this code inside a loop to loop between the two players' turns
 		
 		Console.showBoard(board);
 		
