af3720eaa73767d4c6a183129985462078469f90
aurelienshz <aurelien.schiltz@free.fr>
2016-05-13 13:46:44 +0200

    factorises engine --> methods Game.init() & Game.start()

--- 6Colors/src/edu/isep/sixcolors/SixColors.java
+++ 6Colors/src/edu/isep/sixcolors/SixColors.java
@@ -18,90 +18,12 @@ public class SixColors {
 	 */
 	public static void main(String[] args) {
 		
-		Random random = new Random();
+		// Creating the game controller :
 		Game game = new Game();
 		
-		String name;
-		for(int i = 0; i < game.getPlayers().length; i++) {
-			name = Console.promptPlayerName(i + 1);
-			game.getPlayer(i).setName(name);
-		}
+		game.init();
 		
-		// TODO store board width as an independant parameter
-		
-		Board board = new Board(4);
-		
-		board.getTile(0, 0).setOwner(game.getPlayer(0));
-		
-		while(board.getTile(3, 3).getColor() == board.getTile(0, 0).getColor()) {
-			board.getTile(3, 3).setColor(Color.values()[random.nextInt(Color.values().length)]);
-		}
-		
-		board.getTile(3, 3).setOwner(game.getPlayer(1));
-		
-		// Setting current and previous colors of the players :
-		game.getPlayer(0).setColor(board.getTile(0, 0).getColor());
-		game.getPlayer(0).setPreviousColor(board.getTile(0, 0).getColor());	
-		game.getPlayer(1).setColor(board.getTile(3, 3).getColor());
-		game.getPlayer(1).setPreviousColor(board.getTile(3, 3).getColor());
-		
-		// Updating board to give the players ownership of the tiles of their colors next to their starting point.
-		board.update(0, 0, game.getPlayer(0));
-		board.update(3, 3, game.getPlayer(1));
-		
-		
-		
-		while(true) {
-			Player currentPlayer = game.getCurrentPlayer();
-			System.out.println("It's " + currentPlayer.getName() + "'s turn !");
-			System.out.println("Your current color : "+currentPlayer.getColor().name());
-			
-			Console.showBoard(board);
-			
-			Color chosenColor = Console.promptColorChoice();
-			
-			// TODO refactor those checks smartly :
-			while (chosenColor == currentPlayer.getColor()) {
-				System.out.println("You already control this color");
-				chosenColor = Console.promptColorChoice();
-			}
-			
-			boolean err = true;
-			while (err) {  
-				err = false;
-				for(Player player: game.getPlayers()) {
-					if(player.getColor() == chosenColor) {
-						err = true;
-						System.out.println(player.getName() + " already controls this color. Choose another one.");
-						chosenColor = Console.promptColorChoice();
-					}
-				}
-			}
-			
-			System.out.println("Chosen color : "+chosenColor.name());
-			
-			// updating previous and current colors :
-			// TODO setPrevious private and called in setColor ?
-			currentPlayer.setPreviousColor(currentPlayer.getColor());
-			currentPlayer.setColor(chosenColor);
-			
-			// TODO Player.originTileCoordinates
-			int tileX, tileY;
-			
-			if(game.getCurrentPlayerId() == 0) {
-				tileX = 0;
-				tileY = 0;
-			}
-			else {
-				tileX = 3;
-				tileY = 3;
-			}
-			
-			board.update(tileY, tileX, currentPlayer);
-			
-			// set the current player to the next player :
-			game.nextPlayer();
-		}
+		game.start();
 		
 	}
 
--- 6Colors/src/edu/isep/sixcolors/controller/Game.java
+++ 6Colors/src/edu/isep/sixcolors/controller/Game.java
@@ -1,8 +1,11 @@
 package edu.isep.sixcolors.controller;
+import java.util.Random;
+
 import edu.isep.sixcolors.model.*;
+import edu.isep.sixcolors.view.Console;
 
 /**
- * Game controller : Main controller of the game
+ * Game controller : Controller implementing the game engine
  * Manages inputs and outputs to and from the view
  */
 public class Game {
@@ -11,6 +14,9 @@ public class Game {
 	 */
 	private Player[] players;
 	private int currentPlayerId;
+	private Random random = new Random();
+	private Board board;
+
 	
 	/**
 	 * Create a new game with 2 players
@@ -27,6 +33,91 @@ public class Game {
 		
 		this.currentPlayerId = 0;
 		
+	}
+	
+	/**
+	 * TODO annotate
+	 */
+	public void init() {
+		String name;
+		for(int i = 0; i < getPlayers().length; i++) {
+			name = Console.promptPlayerName(i + 1);
+			getPlayer(i).setName(name);
+		}
+		
+		this.board = new Board(4);
+		
+		board.getTile(0, 0).setOwner(getPlayer(0));
+		
+		while(board.getTile(3, 3).getColor() == board.getTile(0, 0).getColor()) {
+			board.getTile(3, 3).setColor(Color.values()[random.nextInt(Color.values().length)]);
+		}
+		
+		board.getTile(3, 3).setOwner(getPlayer(1));
+		
+		// Setting current and previous colors of the players :
+		getPlayer(0).setColor(board.getTile(0, 0).getColor());
+		getPlayer(0).setPreviousColor(board.getTile(0, 0).getColor());	
+		getPlayer(1).setColor(board.getTile(3, 3).getColor());
+		getPlayer(1).setPreviousColor(board.getTile(3, 3).getColor());
+		
+		// Updating board to give the players ownership of the tiles of their colors next to their starting point.
+		board.update(0, 0, getPlayer(0));
+		board.update(3, 3, getPlayer(1));
+	}
+	
+	public void start() {
+		while(true) {
+			Player currentPlayer = getCurrentPlayer();
+			System.out.println("It's " + currentPlayer.getName() + "'s turn !");
+			System.out.println("Your current color : "+currentPlayer.getColor().name());
+			
+			Console.showBoard(board);
+			
+			Color chosenColor = Console.promptColorChoice();
+			
+			// TODO refactor those checks smartly :
+			while (chosenColor == currentPlayer.getColor()) {
+				System.out.println("You already control this color");
+				chosenColor = Console.promptColorChoice();
+			}
+			
+			boolean err = true;
+			while (err) {  
+				err = false;
+				for(Player player: getPlayers()) {
+					if(player.getColor() == chosenColor) {
+						err = true;
+						System.out.println(player.getName() + " already controls this color. Choose another one.");
+						chosenColor = Console.promptColorChoice();
+					}
+				}
+			}
+			
+			System.out.println("Chosen color : "+chosenColor.name());
+			
+			// updating previous and current colors :
+			// TODO setPrevious private and called in setColor ?
+			currentPlayer.setPreviousColor(currentPlayer.getColor());
+			currentPlayer.setColor(chosenColor);
+			
+			// TODO Player.originTileCoordinates
+			int tileX, tileY;
+			
+			if(getCurrentPlayerId() == 0) {
+				tileX = 0;
+				tileY = 0;
+			}
+			else {
+				tileX = 3;
+				tileY = 3;
+			}
+			
+			board.update(tileY, tileX, currentPlayer);
+			
+			// set the current player to the next player :
+			nextPlayer();
+		}
 	}
 	
 	/**
@@ -47,10 +138,6 @@ public class Game {
 		return this.players;
 	}
 	
-	/**
-	 * Get the current player
-	 * @return Player Current player
-	 */
 	public Player getCurrentPlayer() {
 		return this.players[this.currentPlayerId];
 	}
@@ -59,6 +146,10 @@ public class Game {
 		return this.currentPlayerId;
 	}
 	
+	
+	/**
+	 * Next player up !
+	 */
 	public void nextPlayer() {
 		if(this.currentPlayerId == this.players.length - 1) {
 			this.currentPlayerId = 0;
